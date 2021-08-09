2f524b0c7c2ce1c756ce73397ba805b683252346
Paul Susanin <pasha.susanin15521552@mail.ru>
2021-08-09 09:20:55 +0300

    add (readline): entering amount of the numbers manually (#2)

--- Array v3.php
+++ Array v3.php
@@ -1,15 +1,11 @@
 <?php
-
 $start=microtime(true);
-
-$len = 100000;
+$len=100000; // $len=readline("len="); - the number of elements in the array is entered manually
 
 function ARR($length) {
-
-    $numbers = [];
-    while (count($numbers) < $length) $numbers[$numb] = ($numb = rand(0, $length*10));
-    $values = array_values($numbers);
-
+    $number = [];
+    while (count($number) < $length) $number[$numb] = ($numb = rand(0, $length*10));
+    $values = array_values($number);
     return $values;
 }
 
